32ca8ebdf4d7afa8a3cef5648bac55b3313b3f7e
sjfls2025 <147580345@qq.com>
2025-09-29 09:51:10 +0800

    0929-5

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from 'sonner';
-import App from "./App.tsx";
-import "./index.css";
+import App from "/src/App.tsx";
+import "/src/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
